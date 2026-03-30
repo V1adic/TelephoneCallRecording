6ade5a84a22436950c8d54c2dcd214d3b9614ad0
V1adic <122886131+V1adic@users.noreply.github.com>
2026-03-30 14:50:54 +0500

    Add .gitignore

--- obj/Debug/net10.0/TelephoneCallRecording.AssemblyInfo.cs
+++ obj/Debug/net10.0/TelephoneCallRecording.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelephoneCallRecording")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5928ac03bf9694d897decba3fd7cd70181484136")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed7b44d8a4704fa406568cf347929021d8dd3548")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelephoneCallRecording")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelephoneCallRecording")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
